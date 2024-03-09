83ae5540f1abfb7d672008769d3c86212352eff0
Bram Devlaminck <bram.devlaminck@gmail.com>
2024-03-09 08:34:39 +0100

    overwrite data in array builder to force memory free

--- suffixarray_builder/src/main.rs
+++ suffixarray_builder/src/main.rs
@@ -10,7 +10,8 @@ fn main() -> Result<(), Box<dyn Error>>{
     let Arguments { database_file, taxonomy, output, sample_rate, construction_algorithm } = args;
     let taxon_id_calculator = TaxonIdCalculator::new(&taxonomy, AggregationMethod::LcaStar);
     let data = get_proteins_from_database_file(&database_file, &*taxon_id_calculator)?;
-    let sa = build_sa(&data.input_string, &construction_algorithm, sample_rate)?;
+    let data = &data.input_string; // shadow "data" to force the unused fields being freed
+    let sa = build_sa(data, &construction_algorithm, sample_rate)?;
     write_binary(sample_rate, &sa, &output)?;
     Ok(())
 }

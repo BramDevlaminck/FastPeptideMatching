ca3404dcb17e1399cec801b3564e554b2f2df4a4
Bram Devlaminck <bram.devlaminck@gmail.com>
2024-03-20 22:04:00 +0100

    prevent out of bounds index

--- suffixarray/src/searcher.rs
+++ suffixarray/src/searcher.rs
@@ -72,7 +72,7 @@ impl Searcher {
 
         // TODO: vervang de "of V of K of L, door of V als we alle L'en voor V's zouden vervangen en omgekeerd in de originele tekst
         // Pattern of searching an 'L' is only different from searching a 'I' in 3 cases. This is when the character we compare it with is a 'J', 'K' or 'L'
-        if equalize_i_and_l && index_in_search_string < search_string.len() && search_string[index_in_search_string] == b'I' && (self.proteins.input_string[index] > b'I' && self.proteins.input_string[index] <= b'L') {
+        if equalize_i_and_l && index_in_search_string < search_string.len() && index < self.proteins.input_string.len() && search_string[index_in_search_string] == b'I' && (self.proteins.input_string[index] > b'I' && self.proteins.input_string[index] <= b'L') {
             il_locations.push(index_in_search_string);
         }
 

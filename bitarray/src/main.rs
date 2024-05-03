fcc25bd6789407bdb0ad79de18402e656cfffd36
tibvdm <tibovandemoortele@hotmail.com>
2024-05-03 10:41:52 +0200

    debug main

--- bitarray/src/main.rs
+++ bitarray/src/main.rs
@@ -15,6 +15,8 @@ pub fn main() {
     reader.read_exact(&mut sample_rate_buffer).map_err(|_| "Could not read the sample rate from the binary file").unwrap();
     writer.write(&sample_rate_buffer).unwrap();
 
+    eprintln!("Reading the sample rate from the binary file: {}", sample_rate_buffer[0]);
+
     let size: usize = 29_401_012_224;
     let mut bitarray = BitArray::<37>::with_capacity(size);
     writer.write(&size.to_le_bytes()).unwrap();
@@ -25,6 +27,11 @@ pub fn main() {
     let mut bytes_read = reader.read(&mut buffer).unwrap();
     while bytes_read > 0 {
         for buffer_slice in buffer[..bytes_read].chunks_exact(8) {
+            if index == 0 {
+                eprintln!("value (0) slice: {:?}", buffer_slice);
+                eprintln!("value (0) from_le_bytes: {}", u64::from_le_bytes(buffer_slice.try_into().unwrap()));
+            }
+
             bitarray.set(index, u64::from_le_bytes(buffer_slice.try_into().unwrap()));
             index += 1;
         }

66b4f5a7debf4d58d95289a11fb0d4400e5eb832
tibvdm <tibovandemoortele@hotmail.com>
2024-05-03 11:33:12 +0200

    remove the buffered reader as it might be causing the data loss?

--- bitarray/src/main.rs
+++ bitarray/src/main.rs
@@ -3,16 +3,18 @@ use std::io::{stdin, stdout, BufReader, BufWriter, Read, Write};
 use bitarray::{binary::Binary, BitArray};
 
 pub fn main() {
-    let stdin = stdin();
+    //let mut reader = std::fs::File::open("sa.bin").unwrap();
+
+    let mut stdin = stdin().lock();
     let stdout = stdout();
 
     eprintln!("size of usize: {}", std::mem::size_of::<usize>());
 
-    let mut reader = BufReader::new(stdin.lock());
+    //let mut reader = BufReader::new(file);
     let mut writer = BufWriter::new(stdout.lock());
 
     let mut sample_rate_buffer = [0_u8; 1];
-    reader.read_exact(&mut sample_rate_buffer).map_err(|_| "Could not read the sample rate from the binary file").unwrap();
+    stdin.read_exact(&mut sample_rate_buffer).map_err(|_| "Could not read the sample rate from the binary file").unwrap();
     writer.write(&sample_rate_buffer).unwrap();
 
     eprintln!("Reading the sample rate from the binary file: {}", sample_rate_buffer[0]);
@@ -24,16 +26,16 @@ pub fn main() {
     // this buffer is 1GiB big
     let mut index = 0;
     let mut buffer = [0; 8];
-    let mut bytes_read = reader.read(&mut buffer).unwrap();
+    let mut bytes_read = stdin.read(&mut buffer).unwrap();
 
     //println!("buffer: {:?}", buffer);
 
     while bytes_read > 0 {
         for buffer_slice in buffer[..bytes_read].chunks_exact(8) {
-            // if index == 0 || (index >= 1023 && index <= 1025) {
-            //     eprintln!("value ({}) slice: {:?}", index, buffer_slice);
-            //     eprintln!("value ({}) from_le_bytes: {}", index, u64::from_le_bytes(buffer_slice.try_into().unwrap()));
-            // }
+            if index == 1022 {
+                eprintln!("value ({}) slice: {:?}", index, buffer_slice);
+                eprintln!("value ({}) from_le_bytes: {}", index, u64::from_le_bytes(buffer_slice.try_into().unwrap()));
+            }
 
             if !(buffer[5] == 0 && buffer[6] == 0 && buffer[7] == 0) {
                 eprintln!("value ({}) slice: {:?}", index, buffer_slice);
@@ -49,7 +51,7 @@ pub fn main() {
             break;
         }
 
-        bytes_read = reader.read(&mut buffer).unwrap();
+        bytes_read = stdin.read(&mut buffer).unwrap();
     }
 
     bitarray.write_binary(&mut writer).unwrap();

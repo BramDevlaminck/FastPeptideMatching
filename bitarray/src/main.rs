841f0a548ccd4540c790d32a4fb40923ed2dd42a
tibvdm <tibovandemoortele@hotmail.com>
2024-05-06 16:32:12 +0200

    main from stdin

--- bitarray/src/main.rs
+++ bitarray/src/main.rs
@@ -1,29 +1,35 @@
-use std::io::{stdout, BufWriter, Read, Write};
+use std::io::{stdin, stdout, BufWriter, Read, Write};
 
 use bitarray::{binary::Binary, BitArray};
 
 pub fn main() {
-    let mut reader = std::fs::File::open("sa.bin").unwrap();
-    let mut writer = BufWriter::new(stdout().lock());
+    let stdin = stdin();
+    let stdout = stdout();
 
-    let mut sample_rate_buffer = [0_u8; 1];
-    reader.read_exact(&mut sample_rate_buffer).map_err(|_| "Could not read the sample rate from the binary file").unwrap();
-    writer.write(&sample_rate_buffer).unwrap();
+    let mut reader = stdin.lock().bytes();
+    let mut writer = BufWriter::new(stdout.lock());
 
-    let size: usize = 29_401_012_224;
+    let sample_rate = reader.next().unwrap().unwrap();
+    writer.write(&[sample_rate]).unwrap();
+
+    eprintln!("Reading the sample rate from the binary file: {}", sample_rate);
+
+    let size: usize = 2000;
     let mut bitarray = BitArray::<37>::with_capacity(size);
     writer.write(&size.to_le_bytes()).unwrap();
 
     let mut index = 0;
     let mut buffer = [0; 8];
-    let mut bytes_read = reader.read(&mut buffer).unwrap();
-    while bytes_read > 0 {
-        for buffer_slice in buffer[..bytes_read].chunks_exact(8) {
-            bitarray.set(index, u64::from_le_bytes(buffer_slice.try_into().unwrap()));
+    let mut byte_count = 0;
+    for byte in reader {
+        buffer[byte_count] = byte.unwrap();
+        byte_count += 1;
+
+        if byte_count == 8 {
+            bitarray.set(index, u64::from_le_bytes(buffer));
+            byte_count = 0;
             index += 1;
         }
-
-        bytes_read = reader.read(&mut buffer).unwrap();
     }
 
     bitarray.write_binary(&mut writer).unwrap();
